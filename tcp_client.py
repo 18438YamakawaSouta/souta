27bc5155c81dd046d884ddaabe4638f52daa95c4
18438 <18438@toba-cmt.ac.jp>
2021-11-17 13:12:17 +0900

    なおした

--- tcp_client.py
+++ tcp_client.py
@@ -1,7 +1,7 @@
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    #サーバを指定!
+    #サーバを指定
     s.connect(('10.40.2.26', 50007))
     #サーバにメッセージを送る
     s.sendall(b'hello world')
